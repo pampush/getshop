a7616b5f2e63f57eddac286edcd1cf45ce66e7af
Evgeny <ebmooc@gmail.com>
2021-07-27 22:36:51 +0300

    add banner component

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,13 +4,19 @@ import qrCode from '../assets/images/qr.jpg';
 
 function Banner() {
   return (
-    <div className="banner">
-      <div className="banner__title"></div>
-      <div className="banner__content">
-        <img height="126px" width="126px" src={qrCode}></img>
-        <span className="banner__details"></span>
+    <div className="banner main__banner">
+      <div className="banner__title banner__title_wrapper">
+        Исполните мечту вашего малыша! Подарите ему собаку!
       </div>
-      <div className="banner__controls"></div>
+
+      <div className="banner__content banner__content_wrapper">
+        <img className="banner__image" height="126px" width="126px" src={qrCode}></img>
+        <div className="banner__details">Сканируйте QR-код или нажмите OK</div>
+      </div>
+
+      <a className="banner__submit" tabIndex={1}>
+        OK
+      </a>
     </div>
   );
 }
